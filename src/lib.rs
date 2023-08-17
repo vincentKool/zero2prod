6f8d14be41d84f71495bb13aec4623f5f5c672e3
Vincent Kool <vincentkool@gmail.com>
2023-08-17 17:57:46 +0200

    Health check endpoint is working (end of Ch 3.6)

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,14 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder };
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
+        .run();
+    Ok(server)
 }
